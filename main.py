a4d799124f2f60774334a429c7bd525b2ff6df97
ryzXD <90576128+ryzxdUwU@users.noreply.github.com>
2023-09-09 12:29:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,37 +42,15 @@ def cls():
                                               {COLORS.m}Skidded by ryz{Fore.RESET}
 ''')
 
-with open("config.json", "a+") as f:
-    e = json.load(f)
-
-if e["token"] != "":
-    TOKEN = e["token"]
-else:
-    TOKEN = input("Enter your token: ")
-    e["token"] = TOKEN
-    with open("config.json", "w") as f:
-        json.dump(e, f)
+token = "token"
 
 bot = commands.Bot(command_prefix='!')
 
 global groupchat1_id
 global groupchat2_id
 
-def gcs():
-    with open("config.json", "a+") as f:
-        e = json.load(f)
-        if e["groupchat1_id"] != "" and e["groupchat2_id"] != "":
-            groupchat1_id = e["groupchat1_id"]
-            groupchat2_id = e["groupchat2_id"]
-        else:
-            groupchat1_id = input("gc 1 id: ")
-            groupchat2_id = input("gc 2 id: ") ################ AAAAAAAAAAA kil self
-            e["groupchat1_id"] = groupchat1_id
-            e["groupchat2_id"] = groupchat2_id
-            with open("config.json", "w") as f:
-                json.dump(e, f)
-
-gcs()
+groupchat1_id = id1
+groupchat2_id = id2
 
 @bot.event
 async def on_ready():
